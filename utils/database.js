2fddb4f7b720cf60778d13faa168a12e18c724d3
Uzoma <uzoma.nwanne@gmail.com>
2024-07-05 12:37:57 +0100

    hardcoded db url

--- utils/database.js
+++ utils/database.js
@@ -5,7 +5,7 @@ let _db ;
 const mongoConnect = () => {
   MongoClient.connect(
     'mongodb+srv://uzomanwanne:uzomanwanne@cluster0.nlsnhtn.mongodb.net/webapp'
-  )
+  ) //I did not use .env so that it will be easy for someone to help me out
     .then((client) => {
       console.log("Connected to MongoDB");
       _db = client.db()
